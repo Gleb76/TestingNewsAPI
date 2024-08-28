09d8cefa3872d6d9c5b07fada6cd8325ad9809c8
Gleb Klyga <klyga90@bk.ru>
2024-08-28 17:20:04 +0500

    Made Config file with APIkey and NewsEndpoint

--- TestingNewsAPI/Endpoint/NewsEndpoint.swift
+++ TestingNewsAPI/Endpoint/NewsEndpoint.swift
@@ -8,7 +8,48 @@
 import Foundation
 
 protocol APIBuilder {
-    var urlRequst: URLRequest { get }
+    var urlRequest: URLRequest { get }
     var baseUrl: URL { get }
     var path: String { get }
+    var queryItems: [URLQueryItem] { get }
+}
+
+enum NewsAPI {
+    case getNews(query: String)
+}
+
+extension NewsAPI: APIBuilder {
+    
+    var baseUrl: URL {
+        return URL(string: "https://newsapi.org/v2")!
+    }
+    
+    var path: String {
+        switch self {
+        case .getNews:
+            return "/everything"
+        }
+    }
+    
+    var queryItems: [URLQueryItem] {
+        switch self {
+        case .getNews(let query):
+            let apiKey = ProcessInfo.processInfo.environment["API_KEY"] ?? ""
+            return [
+                URLQueryItem(name: "q", value: query),
+                URLQueryItem(name: "apiKey", value: apiKey)
+            ]
+        }
+    }
+    
+    var urlRequest: URLRequest {
+        var urlComponents = URLComponents(url: baseUrl.appendingPathComponent(path), resolvingAgainstBaseURL: false)
+        urlComponents?.queryItems = queryItems
+        
+        guard let url = urlComponents?.url else {
+            fatalError("Invalid URL")
+        }
+        
+        return URLRequest(url: url)
+    }
 }
