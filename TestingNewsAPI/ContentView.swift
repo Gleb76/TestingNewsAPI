834de07d6c5344401e6fb7973c9cec4946f4d1c9
Gleb Klyga <klyga90@bk.ru>
2024-08-28 16:20:19 +0500

    Initial Commit

---
+++ TestingNewsAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestingNewsAPI
+//
+//  Created by Глеб Клыга on 28.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestingNewsAPI/TestingNewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingNewsAPIApp.swift
+//  TestingNewsAPI
+//
+//  Created by Глеб Клыга on 28.08.24.
+//
+
+import SwiftUI
+
+@main
+struct TestingNewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
